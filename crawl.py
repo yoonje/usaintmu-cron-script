65985e195dd39e44da65adcf6d53b7f736bd3861
yoonje <elxm6123@gmail.com>
2019-08-08 21:37:56 +0900

    finish prototype

--- crawl.py
+++ crawl.py
@@ -1,10 +1,10 @@
 import pysaint
 
 res = pysaint.get("전공", 2019, "2 학기", silent=True)
-pysaint.save_json("./data", "majors.json", res)
+pysaint.save_json("./data", "majors.json", res["2019"]["2 학기"])
 
 res = pysaint.get("교양필수", 2019, "2 학기", silent=True)
-pysaint.save_json("./data", "essentials.json", res)
+pysaint.save_json("./data", "essentials.json", res["2019"]["2 학기"])
 
 res = pysaint.get("교양선택", 2019, "2 학기", silent=True)
 pysaint.save_json("./data", "selectives.json", res["2019"]["2 학기"])
